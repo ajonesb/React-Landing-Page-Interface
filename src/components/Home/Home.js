74b8c2483c4184438a7ec6e255048f7ba48d25d6
alan <alanjones83@gmail.com>
2020-03-13 19:45:40 +1100

    added quick fix for wide screen, removed container-fluid, changed to container, width modification

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,7 @@ import AllTheLatestFromAEG from "../AllTheLatestFromAEG/AllTheLatestFromAEG";
 class Home extends React.Component {
   render() {
     return (
-      <div className="container-fluid">
+      <div className="container">
         <AnswerYourBodysNeeds />;
         <AllTheLatestFromAEG />
       </div>
